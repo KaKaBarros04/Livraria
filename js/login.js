15648d262cf08f62f9cf966ebe96c87c64cc025c
KaKaBarros <kauanbenitez04@gmail.com>
2023-08-15 18:54:51 +0100

    Adicionei o icon de usuario com drop suspenso

--- js/login.js
+++ js/login.js
@@ -5,4 +5,11 @@
         window.location.href = '/resultados?query=' + encodeURIComponent(query);
     });
 
-  
+    // Adicione o evento de clique para abrir/fechar o menu suspenso
+const userIcon = document.querySelector('.user-icon');
+
+userIcon.addEventListener('click', () => {
+    const userDropdown = userIcon.querySelector('.user-dropdown');
+    userDropdown.classList.toggle('show');
+});
+
